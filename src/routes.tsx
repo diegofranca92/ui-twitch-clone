9f97d394d99f978abf1b8f8de7a80071e779bef4
Diego França <diego.franca92@gmail.com>
2021-02-12 14:44:09 -0300

    Inicio do Twitch TV Clone

---
+++ src/routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Following from './pages/Following';
+import ComingSoon from './pages/ComingSoon';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const Routes: React.FC = () => (
+  <NavigationContainer>
+    <Navigator>
+      <Screen name="Home" component={Following} />
+      <Screen name="Teste" component={ComingSoon} />
+      <Screen name="Browse" component={ComingSoon} />
+      <Screen name="Esports" component={ComingSoon} />
+    </Navigator>
+  </NavigationContainer>
+);
+
+export default Routes;
